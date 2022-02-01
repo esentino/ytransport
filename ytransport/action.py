a58ca043d0feb10bc2d75f7cf13171f4815a16d9
Janusz Janowski <januszjanowski@outlook.com>
2022-02-01 23:44:42 +0100

    add first action buy_truck

---
+++ ytransport/action.py
@@ -0,0 +1,26 @@
+from tortoise.expressions import F
+from tortoise.transactions import atomic
+
+from ytransport.models import Player, PlayerTruck, Truck
+
+
+class NotEnoughCashForBuyTruck(Exception):
+    def __init__(self, price: float, money: float):
+        self._price = price
+        self._money = money
+
+    def __str__(self):
+        return f"Not enough cash for buy truck ({self._price}$) missing {self._price-self._money}$."
+
+
+@atomic
+async def buy_truck(truck: Truck, player: Player) -> PlayerTruck:
+    await player.refresh_from_db()
+
+    if truck.price >= player.money:
+        raise NotEnoughCashForBuyTruck(truck.price, player.money)
+
+    player.money = F("money") - truck.price
+    await player.save()
+
+    return await PlayerTruck.create(truck=Truck, player=Player, load=0)
